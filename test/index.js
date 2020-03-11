c35e2064d339ad827a40a2b25e6cf144ad12d8aa
Joseph Sandmeyer <joey.sandmeyer@gmail.com>
2020-03-11 13:47:45 -0700

    Create test suite

---
+++ test/index.js
@@ -0,0 +1,3 @@
+describe('Bare Minimum Requirements', function() {
+  require('./tic-tac-toe/game.test.js');
+});
---
+++ test/tic-tac-toe/game.test.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect;
+
+describe('', function() {
+  var game = require('../../game.js');
+
+  describe('', function() {
+
+    it('should ', function() {
+
+      expect();
+    });
+
+  });
+
+});
